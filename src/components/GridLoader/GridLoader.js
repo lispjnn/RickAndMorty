55eb43e1b1d4765df5d35c3be1e3a4f32e9a3274
Joanna <lispjnn@gmail.com>
2024-09-21 02:25:09 -0400

    modify grid loader layout

--- src/components/GridLoader/GridLoader.js
+++ src/components/GridLoader/GridLoader.js
@@ -2,12 +2,17 @@ import './GridLoader.css';
 
 export default function GridLoader() {
     return(
-        Array.from({ length: 10 }, (_, idx) => (
-            <div key={idx} className="skeleton-card">
-                <div className="skeleton-text"></div>
-                <div className="skeleton-text"></div>
-                <div className="skeleton-shimmer"></div>
-            </div>
-        ))
+        <div className="grid">
+            {
+                Array.from({ length: 10 }, (_, idx) => (
+                    <div key={idx} className="skeleton-card">
+                        <div className="skeleton-text"></div>
+                        <div className="skeleton-text"></div>
+                        <div className="skeleton-shimmer"></div>
+                    </div>
+                ))
+            }
+        </div>
+        
     )
 }
