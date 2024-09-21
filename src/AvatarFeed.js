accc90df90cb620d2d3dde512643a6825157053d
Joanna <lispjnn@gmail.com>
2024-09-20 21:42:36 -0400

    replace filter/sort buttons with icon buttons

--- src/AvatarFeed.js
+++ src/AvatarFeed.js
@@ -1,17 +1,20 @@
 import { useEffect, useState } from "react";
 import AvatarCard from "./AvatarCard";
-export default function AvatarFeed(){
+import 'font-awesome/css/font-awesome.min.css'; // Import Font Awesome
+
+export default function AvatarFeed() {
     const [totalPages, setTotalPages] = useState(0);
     const [avatarData, setAvatarData] = useState([]);
-    const [filter, setFilter] = useState('All');
+    const [filter, setFilter] = useState('Any');
     const [sort, setSort] = useState('Oldest');
+
     // Fetch total pages 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
-        .then((res) => res.json())
-        .then((data) => {
-            setTotalPages(data.info.pages);  // Set the total number of pages
-        });
+            .then((res) => res.json())
+            .then((data) => {
+                setTotalPages(data.info.pages);  // Set the total number of pages
+            });
     }, []);
 
     // Fetch characters from all pages
@@ -20,10 +23,10 @@ export default function AvatarFeed(){
             let allCharacters = [];
             for (let i = 1; i <= totalPages; i++) {
                 await fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
-                .then((res) => res.json())
-                .then((data) => {
-                    allCharacters = [...allCharacters, ...data.results];  // Merge results with existing character data
-                });
+                    .then((res) => res.json())
+                    .then((data) => {
+                        allCharacters = [...allCharacters, ...data.results];  // Merge results with existing character data
+                    });
             }
             setAvatarData(allCharacters);  // Update state after fetching all characters
         };
@@ -31,64 +34,77 @@ export default function AvatarFeed(){
         if (totalPages > 0) {  // Ensure totalPages is set before fetching characters
             fetchCharacters();
         }
-
     }, [totalPages]);
 
     // Sorts avatars either Oldest-Newest or Newest-Oldest
     const sortedAvatars = avatarData.sort((a, b) => {
         const dateA = new Date(a.created);
         const dateB = new Date(b.created);
-        if(sort === "Oldest"){
+        if (sort === "Oldest") {
             return dateA - dateB;
         }
         return dateB - dateA;
-    })
+    });
 
     // Filters avatars based on selected status filter
     const filteredAvatars = sortedAvatars.filter((avatar) => {
-        if(filter === 'All') return true;
+        if (filter === 'Any') return true;
         return avatar.status === filter;
-    })
+    });
 
-    return(
-       <div>
-        <div className="dropdown">
-            <button className="dropdownButton">Filter</button>
-            <div className="dropdown-content">
-            <div onClick={() => setFilter('Alive')}>Alive</div>
-                <div onClick={() => setFilter('Dead')}>Dead</div>
-                <div onClick={() => setFilter('unknown')}>Unknown</div>
-                <div onClick={() => setFilter('All')}>All</div> 
+    return (
+        <div>
+            <div className="refineBar">
+
+                {/* Filter dropdown menu */}
+                <div className="dropdown">
+                    <button className="dropdownButton">
+                        <i className="fa fa-filter"></i>
+                    </button>
+                    <div className="dropdown-content">
+                        <div onClick={() => setFilter('Alive')}>Alive</div>
+                        <div onClick={() => setFilter('Dead')}>Dead</div>
+                        <div onClick={() => setFilter('unknown')}>Unknown</div>
+                        <div onClick={() => setFilter('Any')}>Any</div>
+                    </div>
+                </div>
+
+                {/* Sort dropdown menu */}
+                <div className="dropdown">
+                    <button className="dropdownButton">
+                        <i className="fa fa-sort"></i>
+                    </button>
+                    <div className="dropdown-content">
+                        <div onClick={() => setSort('Newest')}>Newest to Oldest</div>
+                        <div onClick={() => setSort('Oldest')}>Oldest to Newest</div>
+                    </div>
+                </div>
+                
+                {/* Display current filter and sort */}
+                <div className="current-selections">
+                    <span className="tag">Status: {filter}</span>
+                    <span className="tag">Sort by: {sort}</span>
+                </div>
+            </div>
+            
+            {/* Display cards */}
+            <div className="cardContainer">
+                {filteredAvatars.length > 0 ? (
+                    filteredAvatars.map((avatar) => (
+                        <AvatarCard
+                            key={avatar.id}
+                            name={avatar.name}
+                            species={avatar.species}
+                            status={avatar.status}
+                            gender={avatar.gender}
+                            created={avatar.created}
+                            img={avatar.image}
+                        />
+                    ))
+                ) : (
+                    <p>No characters found</p>
+                )}
             </div>
         </div>
-
-        <div className="dropdown">
-            <button className="dropdownButton">Sort</button>
-            <div className="dropdown-content">
-                <div onClick={() => setSort('Newest')}>Newest to Oldest</div>
-                <div onClick={() => setSort('Oldest')}>Oldest to Newest</div>
-            </div>
-        </div>
-
-        <div className="cardContainer">
-            {filteredAvatars.length > 0 ? (
-                filteredAvatars.map((avatar) => (
-                    <AvatarCard
-                        key={avatar.id}
-                        name={avatar.name}
-                        species={avatar.species}
-                        status={avatar.status}
-                        gender={avatar.gender}
-                        created={avatar.created}
-                        img={avatar.image}
-                    />
-                ))
-            ) : (
-                <p>No characters found</p> 
-            )}
-        </div>
-       </div>
-        
-    )
-    
+    );
 }
