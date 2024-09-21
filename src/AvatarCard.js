1925bcb79617a070a3b0e219f95ed5a556741969
Joanna <lispjnn@gmail.com>
2024-09-20 21:43:24 -0400

    modify styling

--- src/AvatarCard.js
+++ src/AvatarCard.js
@@ -3,7 +3,7 @@ export default function AvatarCard({ name, species, status, gender, created, img
     return(
         <div className="card">
             <img src={img}></img>
-            <h1>{name}</h1>
+            <h2>{name}</h2>
             <div>Species: {species}</div>
             <div>Status: {status}</div>
             <div>Gender: {gender}</div>
